79d5d3255209489f2db09fed9ce00cc07c1df571
Cristian Henrici <henricri@students.zhaw.ch>
2020-06-29 21:18:04 +0200

    changes to pp file

--- site-modules/dbplayground/plans/postgres.pp
+++ site-modules/dbplayground/plans/postgres.pp
@@ -4,9 +4,6 @@ plan dbplayground::postgres (
      $targets.apply_prep
      apply($targets) {
 	class { 'postgresql::server':
-  ip_mask_deny_postgres_user => '0.0.0.0/32',
-  ip_mask_allow_all_users    => '0.0.0.0/0',
-  ipv4acls                   => ['hostssl all johndoe 192.168.0.0/24 cert'],
   postgres_password          => 'cre123',
 }
         }
