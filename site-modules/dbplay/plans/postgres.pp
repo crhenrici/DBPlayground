6e0a7b3d9af0dc11205464f1c7aa8aca3f284ddf
Christoph Henrici <chhenrici@gmail.com>
2020-07-05 16:53:33 +0200

    Port

--- site-modules/dbplay/plans/postgres.pp
+++ site-modules/dbplay/plans/postgres.pp
@@ -4,6 +4,7 @@ plan dbplay::postgres(
   $targets.apply_prep
   apply($targets) {
     class { 'postgresql::server':
+      ip_mask_allow_all_users    => '172.16.0.0/16',
       postgres_password => 'test123',
     }
   }
