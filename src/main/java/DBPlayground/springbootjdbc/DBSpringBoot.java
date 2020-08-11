9f95684f78423ae7817b84ae0d3e3d34b8941367
Cristian Henrici <henricri@students.zhaw.ch>
2020-08-11 14:59:14 +0200

    went through spring jdbc tutorial with mariadb

--- src/main/java/DBPlayground/springbootjdbc/DBSpringBoot.java
+++ src/main/java/DBPlayground/springbootjdbc/DBSpringBoot.java
@@ -33,13 +33,14 @@ public class DBSpringBoot implements CommandLineRunner {
     }
 
     private void update() {
-        String updateSQL = "UPDATE test_table SET testdesc = 'some test in java spring boot' WHERE testId = 4;";
+        String updateSQL = "UPDATE test_table SET testdesc = 'some new test in java spring boot' WHERE testId = 4;";
         jdbcTemplate.execute(updateSQL);
     }
 
     private void select() {
         String selectSQL = "SELECT * FROM test_table";
-        jdbcTemplate.query(selectSQL, new RowCallbackHandler() {
+        jdbcTemplate.query(selectSQL, new
+                RowCallbackHandler() {
             @Override
             public void processRow(ResultSet rs) throws SQLException {
                 System.out.println(rs.getInt(1));
@@ -49,7 +50,7 @@ public class DBSpringBoot implements CommandLineRunner {
     }
 
     private void insert() {
-        String insertSQL = "INSERT INTO test_table (testdesc) VALUES ('Test in Java Spring Boot');";
+        String insertSQL = "INSERT INTO test_table (testdesc) VALUES ('Some new Test in Java Spring Boot');";
         jdbcTemplate.execute(insertSQL);
     }
 
